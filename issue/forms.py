0df3068f0c847d9f31ab138e3278c602534b0f4e
Brett Monson <hxr@outlook.com>
2023-03-12 00:12:58 -0700

    status buttons with agent perms and test cases

--- issue/forms.py
+++ issue/forms.py
@@ -5,19 +5,22 @@ from .models import Issue
 class CreateIssueForm(forms.ModelForm):
     class Meta:
         model = Issue
-        fields = ['brief','description','subcategory']
+        fields = ['brief','description','subcategory','student','escalation']
         widgets = {
             'brief': forms.TextInput(attrs={'class': 'form-control'}),
             'description': forms.Textarea(attrs={'class': 'form-control'}),
             'subcategory': forms.Select(attrs={'class': 'form-select'}),
+            'student': forms.HiddenInput(),
+            'escalation': forms.TextInput(),
         }
 
 class UpdateIssueForm(forms.ModelForm):
     class Meta:
         model = Issue
-        fields = ['brief','description','subcategory']
+        fields = ['brief','description','subcategory','student']
         widgets = {
             'brief': forms.TextInput(attrs={'class': 'form-control'}),
             'description': forms.Textarea(attrs={'class': 'form-control'}),
             'subcategory': forms.Select(attrs={'class': 'form-select'}),
+            'student': forms.HiddenInput(attrs={'readonly': 'readonly'}),
         }
--- issue/mixins.py
+++ issue/mixins.py
@@ -5,13 +5,35 @@ class GetterMixin:
 
     @classmethod
     def NewId(cls):
-        obj, _ = cls.objects.get_or_create(name="New")
-        return obj.id
+        obj = cls.objects.filter(name="New").first()
+        return obj.id if obj else 1
+
+    @classmethod
+    def New(cls):
+        return cls.objects.filter(name="New").first()
+    
+    @classmethod
+    def ResolvedId(cls):
+        obj = cls.objects.filter(name="Resolved").first()
+        return obj.id if obj else 1
+    
+    @classmethod
+    def Resolved(cls):
+        return cls.objects.filter(name="Resolved").first()
     
     @classmethod
     def ClosedId(cls):
-        obj, _ = cls.objects.get_or_create(name="Closed")
-        return obj.id
+        obj = cls.objects.filter(name="Closed").first()
+        return obj.id if obj else 1
+    
+    @classmethod
+    def EscalatedId(cls):
+        obj = cls.objects.filter(name="Escalated").first()
+        return obj.id if obj else 1
+    
+    @classmethod
+    def Closed(cls):
+        return cls.objects.filter(name="Closed").first()
 
     ###############
     ### SUBCATEGORY 
@@ -19,8 +41,8 @@ class GetterMixin:
 
     @classmethod
     def GeneralId(cls):
-        obj, _ = cls.objects.get_or_create(name="General")
-        return obj.id
+        obj = cls.objects.filter(name="General").first()
+        return obj.id if obj else 1
 
     ###############
     ### REASON
@@ -28,8 +50,8 @@ class GetterMixin:
     
     @classmethod
     def CreatedId(cls):
-        obj, _ = cls.objects.get_or_create(name="Created")
-        return obj.id
+        obj = cls.objects.filter(name="Created").first()
+        return obj.id if obj else 1
     
     ###############
     ### GROUP
@@ -37,6 +59,28 @@ class GetterMixin:
     
     @classmethod
     def Level1Id(cls):
-        obj, _ = cls.objects.get_or_create(name="Level1")
-        return obj.id
+        obj = cls.objects.filter(name="Level1").first()
+        return obj.id if obj else 1
+    
+    @classmethod
+    def Level1(cls):
+        return cls.objects.filter(name="Level1").first()
+    
+    @classmethod
+    def Level2Id(cls):
+        obj = cls.objects.filter(name="Level2").first()
+        return obj.id if obj else 1
+    
+    @classmethod
+    def Level2(cls):
+        return cls.objects.filter(name="Level2").first()
+    
+    @classmethod
+    def Level3Id(cls):
+        obj = cls.objects.filter(name="Level3").first()
+        return obj.id if obj else 1
+    
+    @classmethod
+    def Level3(cls):
+        return cls.objects.filter(name="Level3").first()
     
